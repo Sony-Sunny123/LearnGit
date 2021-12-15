c864bce7ea53f806b41c6ad1e94585e68d632967
sonys <sonys@DESKTOP-LC8G8L6>
2021-12-15 16:00:31 +0530

    learn git

---
+++ src/test/java/com/crm/learngitandjenkins/LearnGitTest.java
@@ -0,0 +1,13 @@
+package com.crm.learngitandjenkins;
+
+import org.testng.annotations.Test;
+
+public class LearnGitTest {
+	@Test
+	public void learngitTest()
+	{
+		System.out.println("hi learn git and jenkins");
+	}
+	
+
+}
